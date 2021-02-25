0d275eaa8e53c700c211f0fe9857a075914839c8
Stephen Dampier <steveoh@fishyfish.com>
2021-02-25 14:40:25 -0800

    trying to add error handling that works along with e.prevent.default. Whacky

--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -1,24 +1,43 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import { ESRCH } from 'constants';
 
 const ProductForm = () => {
     //keep track of what is being typed via useState hook
     const [title, setTitle] = useState("");
     const [price, setPrice] = useState("");
     const [description, setDescription] = useState("");
+    const [errs, setErrs] = useState({});
+
     //handler when the form is submitted
     const onSubmitHandler = e => {
         //prevent default behavior of the submit
         //e.preventDefault();
         // but it prevents the page getting a product added to dom. comment out and it works fine
         //make a post request to create a new product
+        const newProduct = {
+            title:title,
+            price:price,
+            description:description,
+        }
+        console.log(newProduct);
+        
         axios.post('http://localhost:8000/api/products/', {
             title,
             price,
             description,      
         })
-            .then(res=>console.log(res))
-            .catch(err=>console.log(err))
+        .then((response) =>{
+            if (response.data.errors){
+                console.log(response.data.errors);
+                setErrs(response.data.errors);
+            } else {
+            console.log(response.data);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
     };
     
     //onChange to update firstName and lastName
@@ -30,14 +49,29 @@ const ProductForm = () => {
             <p>
                 <label>Title</label>
                 <input type="text" placeholder="Title" onChange = {(e)=>setTitle(e.target.value)}/>
+                {
+                    errs.title ?
+                    <span className="error-text">{errs.title.message}</span>
+                    :null
+                }
             </p>
             <p>
                 <label>Price</label>
                 <input type="text" placeholder="Price" onChange = {(e)=>setPrice(e.target.value)}/>
+                {
+                    errs.price ?
+                    <span className="error-text">{errs.price.message}</span>
+                    :null
+                }
             </p>
             <p>
                 <label>Description</label>
                 <input type="text" placeholder="Description" onChange = {(e)=>setDescription(e.target.value)}/>
+                {
+                    errs.description ?
+                    <span className="error-text">{errs.description.message}</span>
+                    :null
+                }
             </p>
 
             <button className="toggle-me" type="submit">Create</button>
--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
-    title: { type: String },
-    price: { type: Number},
-    description: { type: String },
+    title: { 
+        type: String,
+        required: [true, "You must have a title."],
+        minlength: [3, "Your title must be at least 3 characters long"],
+    },
+    price: { 
+        type: String,
+        required: [true, "You include a price."],
+        minlength: [2, "Your price must be at least 2 characters long, and must be a number."],
+    },
+    description: { 
+        type: String,
+        required: [true, "You must include a description."],
+        minlength: [3, "Your description must be at least 3 characters long."],
+    },
 }, { timestamps: true });
 module.exports = mongoose.model('Product', ProductSchema);
